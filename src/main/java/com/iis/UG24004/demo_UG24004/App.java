9c131d254b29e69df236b8a7a1b4b9476fdf072d
edward191919 <ug24004@ues.edu.sv>
2024-05-05 21:53:33 -0600

    se agrega el proyecto

---
+++ src/main/java/com/iis/UG24004/demo_UG24004/App.java
@@ -0,0 +1,17 @@
+package com.iis.UG24004.demo_UG24004;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "hello world";
+	}
+    public static void main( String[] args )
+    {
+    	App hw= new App();
+        System.out.println( hw.Hello() );
+    }
+}
---
+++ src/test/java/com/iis/UG24004/demo_UG24004/AppTest.java
@@ -0,0 +1,19 @@
+package com.iis.UG24004.demo_UG24004;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
